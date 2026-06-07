4133bfb4cffc83c180308ef170572f2358b49907
Vipinkumar73 <vk03192006@gmail.com>
2026-06-07 18:18:38 +0530

    DAY3 Assignment is uploaded

---
+++ DAY3/q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,count=0;
+    printf("enter a number");
+    scanf("%d",&n);
+
+    while(n>0)
+    {
+
+    
+    n=n/10;
+    count++;
+
+    }
+    printf("total %d digit",count);
+    return 0;
+
+
+}
